6d1065e746649b13721dac7109ad76d4204f8737
qhw <1039954093@qq.com>
2021-05-29 10:16:59 +0800

    [style] re-format code with isort and black

--- index.py
+++ index.py
@@ -1,83 +1,94 @@
-from flask import Flask, request, render_template, send_from_directory, jsonify
-from flask_cors import CORS
-import leancloud
 import hashlib
-import time
 import json
 import os
+import time
+
+import leancloud
+from flask import Flask, render_template, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-LCID = os.environ['APPID']
-LCKEY = os.environ['APPKEY']
+LCID = os.environ["APPID"]
+LCKEY = os.environ["APPKEY"]
 
 m = hashlib.md5()
 m.update((LCID + LCKEY).encode())
 CKID = m.hexdigest() + "_usrid"
 
 leancloud.init(LCID, LCKEY)
-OrLike = leancloud.Object.extend('OrLike')
+OrLike = leancloud.Object.extend("OrLike")
 
-@app.route('/', methods=["GET"])
+
+@app.route("/", methods=["GET"])
 def style():
     return render_template("test.html", server="")
 
-@app.route('/tmp', methods=["GET"])
-def sdtmp():
-    func = request.args.get('callback')
-    return func + "(" + json.dumps({'stat':'ok', 'template': render_template("orlike.html")}) + ")"
 
-@app.route('/orl', methods=["GET"])
+@app.route("/tmp", methods=["GET"])
+def sdtmp():
+    func = request.args.get("callback")
+    return (
+        func
+        + "("
+        + json.dumps({"stat": "ok", "template": render_template("orlike.html")})
+        + ")"
+    )
+
+
+@app.route("/orl", methods=["GET"])
 def orl():
-    method = request.args.get('method')
-    func = request.args.get('callback')
-    if (method != 'like' and method != 'dislike'):
-        return func + "(" + json.dumps({'stat': 'failed'}) + ")"
-    link = request.args.get('link')
+    method = request.args.get("method")
+    func = request.args.get("callback")
+    if method != "like" and method != "dislike":
+        return func + "(" + json.dumps({"stat": "failed"}) + ")"
+    link = request.args.get("link")
     uid = request.args.get(CKID)
 
     query = OrLike.query
-    query.equal_to('method', method)
-    query.equal_to('link', link)
-    query.equal_to('uid', uid)
+    query.equal_to("method", method)
+    query.equal_to("link", link)
+    query.equal_to("uid", uid)
     print(method, link, uid)
     exist = query.find()
     if not exist:
         orlike = OrLike()
-        orlike.set('method', method)
-        orlike.set('link', link)
-        orlike.set('uid', uid)
+        orlike.set("method", method)
+        orlike.set("link", link)
+        orlike.set("uid", uid)
         orlike.save()
     else:
         [e.destroy() for e in exist]
-    return func + "(" + json.dumps({'stat': 'ok', 'uid': uid}) + ")"
+    return func + "(" + json.dumps({"stat": "ok", "uid": uid}) + ")"
 
 
-@app.route('/qry', methods=["GET"])
+@app.route("/qry", methods=["GET"])
 def qry():
-    link = request.args.get('link')
+    link = request.args.get("link")
     query = OrLike.query
-    query.equal_to('method', "like")
-    query.equal_to('link', link)
+    query.equal_to("method", "like")
+    query.equal_to("link", link)
     cnt_like = query.count()
-    query.equal_to('method', "dislike")
-    query.equal_to('link', link)
+    query.equal_to("method", "dislike")
+    query.equal_to("link", link)
     cnt_dislike = query.count()
-    response = {'stat': 'ok', 'like': cnt_like, 'dislike': cnt_dislike}
-    func = request.args.get('callback')
+    response = {"stat": "ok", "like": cnt_like, "dislike": cnt_dislike}
+    func = request.args.get("callback")
     return func + "(" + json.dumps(response) + ")"
 
 
-@app.route('/ckusr', methods=["GET"])
+@app.route("/ckusr", methods=["GET"])
 def ckusr():
-    response = {'stat': 'ok', 'ckid': CKID}
+    response = {"stat": "ok", "ckid": CKID}
     td = str(time.time())
     m = hashlib.md5()
     m.update((td + request.remote_addr).encode())
-    response['uid'] = m.hexdigest()
-    func = request.args.get('callback')
+    response["uid"] = m.hexdigest()
+    func = request.args.get("callback")
     return func + "(" + json.dumps(response) + ")"
 
+
 if __name__ == "__main__":
+
     app.run()
